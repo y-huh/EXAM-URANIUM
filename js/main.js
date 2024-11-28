fd620a1357eaa1ce8880ddf4da1e1fcf456a632d
unknown <huhmlbb@gmail.com>
2024-11-28 19:59:45 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//5
+// let numbers = [346, -45, 565, -652, 887, -244]
+// let musbat = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 0) {
+//         musbat.push(numbers[i])
+//     }        
+// }
+
+// console.log("Musbat qiymatli array: ", musbat);
+
+//6
+// let int = prompt("3 xonali son kiriting")-0
+
+// let onlik = Math.floor((int/10)%10)
+
+// console.log("Berilgan sonning o'nligi", onlik , "ga teng");
+
+//7
+// let arr1 = [5, 4, 3, 6, 3, 7, 1];
+// let arr2 = [22, 1, 42, 34, 55, 67, 11, 32];
+// let sort = [...arr1, ...arr2].sort((a, b) => a - b);
+// console.log("Sonlar tartibi: ", sort);
+
+//8
+// let size = prompt("Son kiriting:");
+// let array = [];
+// for (let i = 0; i < size; i++) {
+//     let name = prompt("Ism kiriting :")
+//     array.push(name)
+// }
+// console.log("Hamma ismlar:", array);
